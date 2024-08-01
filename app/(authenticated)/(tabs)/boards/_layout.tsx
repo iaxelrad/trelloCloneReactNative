ef7ba0e2c12508203bbb3f4f38cfb58adaa750bd
Itamar <iaxelrad@gmail.com>
2024-08-01 21:51:57 +0300

    Building the authenticated Tab Bar Layout

---
+++ app/(authenticated)/(tabs)/boards/_layout.tsx
@@ -0,0 +1,25 @@
+import { Colors } from '@/constants/Colors';
+import { Stack } from 'expo-router';
+import React from 'react';
+import { Image } from 'react-native';
+
+const Layout = () => {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerStyle: { backgroundColor: Colors.primary },
+          headerTitle: () => (
+            <Image
+              style={{ width: 120, height: 50, resizeMode: 'contain' }}
+              source={require('@/assets/images/trello-logo-gradient-white.png')}
+            />
+          ),
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default Layout;
