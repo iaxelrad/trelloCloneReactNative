6d8087e73c5ebdac9fae210a9a5b9df97e041e5a
Itamar <iaxelrad@gmail.com>
2024-08-06 21:46:49 +0300

    Upload Images to supabase Storage

--- components/Board/ListItem.tsx
+++ components/Board/ListItem.tsx
@@ -1,14 +1,26 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
 import {
   RenderItemParams,
   ScaleDecorator,
 } from 'react-native-draggable-flatlist';
 import { Task } from '@/types/enums';
 import { useRouter } from 'expo-router';
+import { useSupabase } from '@/context/SupabaseContext';
+import { Ionicons } from '@expo/vector-icons';
 
 const ListItem = ({ item, drag, isActive }: RenderItemParams<Task>) => {
   const router = useRouter();
+  const { getFileFromPath } = useSupabase();
+  const [imagePath, setImagePath] = useState<string>();
+
+  if (item.image_url) {
+    getFileFromPath!(item.image_url).then(path => {
+      if (path) {
+        setImagePath(path);
+      }
+    });
+  }
 
   const openLink = () => {
     router.push(`/board/card/${item.id}`);
@@ -27,7 +39,29 @@ const ListItem = ({ item, drag, isActive }: RenderItemParams<Task>) => {
             <Text>{item.title}</Text>
           </View>
         ) : (
-          <></>
+          <>
+            {imagePath && (
+              <Image
+                source={{ uri: imagePath }}
+                style={{
+                  width: '100%',
+                  height: 200,
+                  borderRadius: 4,
+                  backgroundColor: '#fff',
+                }}
+              />
+            )}
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Text style={{ flex: 1 }}>{item.title}</Text>
+              {item.assigned_to && (
+                <Ionicons
+                  name="person-circle-outline"
+                  size={16}
+                  color="black"
+                />
+              )}
+            </View>
+          </>
         )}
       </TouchableOpacity>
     </ScaleDecorator>
